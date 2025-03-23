d8a5161a77d36de61a1aece59ebbd6d39be42d7e
Peeter <pratchaya.maneechot@gmail.com>
2025-03-23 12:43:08 +0700

    feat(gateway-service): implement user login with context (authentication)

--- apps/gateway-service/src/graphql/context.ts
+++ apps/gateway-service/src/graphql/context.ts
@@ -1,11 +1,26 @@
+import { AuthProvider, IAuthProvider } from '@restaurant/core-domain';
+import { logger } from '@restaurant/shared-utils';
 import express from 'express';
 import { grpcClient as reservationGrpcClient } from '../services/reservation.service';
 import { grpcClient as userGrpcClient } from '../services/user.service';
-import { IAppContext } from './types';
+import { IAppContext, IAuthentication } from './types';
 
-export async function createContext(_req: express.Request): Promise<IAppContext> {
+export async function createContext(req: express.Request): Promise<IAppContext> {
+  const identity: IAuthentication = {
+    id: '',
+  };
+  try {
+    const idToken = req.headers.authorization?.replace('Bearer ', '');
+    if (idToken) {
+      const authService: IAuthProvider = new AuthProvider();
+      const verified = await authService.verifyToken<IAuthentication>(idToken);
+      identity.id = verified.id;
+    }
+  } catch (error) {
+    logger.error(error, req.headers);
+  }
   return {
-    identity: { id: '' },
+    identity,
     service: {
       user: userGrpcClient,
       reservation: reservationGrpcClient,
--- apps/gateway-service/src/graphql/resolvers/users.ts
+++ apps/gateway-service/src/graphql/resolvers/users.ts
@@ -1,13 +1,26 @@
-import { RegisterUserResponse__Output } from '@restaurant/shared-proto-ts';
+import { LoginUserResponse__Output, RegisterUserResponse__Output } from '@restaurant/shared-proto-ts';
 import { IAppContext } from '../types';
 
 const userResolver = {
   Mutation: {
+    login: async (_parent, args, ctx: IAppContext) => {
+      const resp = await new Promise<LoginUserResponse__Output>((resolve, reject) => {
+        ctx.service.user.LoginUser(args.input, (err, value) => {
+          if (err) {
+            reject(err);
+          } else if (value) {
+            resolve(value);
+          } else {
+            reject(new Error('No value from response'));
+          }
+        });
+      });
+      return resp;
+    },
     register: async (_parent, args, ctx: IAppContext) => {
       const resp = await new Promise<RegisterUserResponse__Output>((resolve, reject) => {
         ctx.service.user.RegisterUser(args.input, (err, value) => {
           if (err) {
-            console.error('gRPC Error:', err);
             reject(err);
           } else if (value) {
             resolve(value);
